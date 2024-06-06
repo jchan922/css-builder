db16caf92a330571fac84f48a1e488deef5e2a0b
Justin Chan <jchan922@gmail.com>
2024-06-06 15:26:17 -0700

    finish tutuorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3000 },
+  server: {
+    port: 3000
+  },
+  integrations: [preact()]
 });
